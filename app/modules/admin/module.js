cf5e97b657225f23ef56b4cee007a6d8dac22653
dustin zhang <dike.zhang@gmail.com>
2016-02-01 07:31:11 -0500

    added admin

---
+++ app/modules/admin/module.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('spBlogger.admin').config(
+		[ '$stateProvider', function($stateProvider) {
+			$stateProvider.state('admin', {
+				url : '/admin',
+				abstract : true,
+				controller : 'AdminController',
+				templateUrl : 'modules/admin/views/admin-home.html'
+			}).state('admin.postNew', {
+				url : '/posts/new',
+				controller : 'PostCreationController',
+				templateUrl : 'modules/admin/views/admin-new-post.html'
+			}).state('admin.postUpdate', {
+				url : '/posts/:id/edit',
+				controller : 'PostUpdateController',
+				templateUrl : 'modules/admin/views/admin-update-post.html'
+			}).state('admin.postViewAll', {
+				url : '',
+				controller : 'PostListController',
+				templateUrl : 'modules/admin/views/admin-all-posts.html'
+			});
+		} ]);
